46c6dc5138444077398ad95c977ea071f713cafe
Laska <adenworker@gmail.com>
2022-05-29 00:24:17 +0300

    sout branch commit 2

--- src/test/java/guru/qa/SoutTest.java
+++ src/test/java/guru/qa/SoutTest.java
@@ -2,6 +2,6 @@ package guru.qa;
 
 public class SoutTest {
     public static void main (String[] args) {
-        System.out.println("Hi QAguru 1");
+        System.out.println("Hi QAguru 2");
     }
 }
